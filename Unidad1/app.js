ce3f02406604ef8674d8078d19eb2164b29465f2
JorgeMedina <JorgeMedina@gmail.com>
2023-09-21 17:27:09 +0200

    confirmar

---
+++ Unidad1/app.js
@@ -0,0 +1,27 @@
+console.log("Hola Mundo"); //Mensaje general
+console.info('Esto es un mensaje'); //Informacion
+console.warn('Cuidadin') //Aviso
+console.error('Error fatal') //Error
+//alert('Mensaje');
+//confirm('Confirmación');
+prompt('mensaje');
+var num=5; //variable tradicional visible en todo el script
+let numero=8; //variable accesible sólo a su scope
+const pi=3.14;//constante accesible a su scope
+/*
+Number: Número
+String: cadena
+Boolean:
+NaN: Not a number
+Undefined: No definido
+Null: nulo
+*/
+/*
++ - * /
+*/
+/* Operadores de comparacion
+<>
+<= >=
+== !=
+=== !==
+*/
